20cb5b0a14f2dfc21b3885f49c5fd05ddb483dce
glloy063 <164896917+glloy063@users.noreply.github.com>
2024-03-25 10:26:09 -0400

    Add files via upload

---
+++ Volunteerscript.js
@@ -0,0 +1,9 @@
+function openDonationPopup() {
+    document.getElementById("donationPopup").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeDonationPopup() {
+    document.getElementById("donationPopup").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
---
+++ script.js
@@ -0,0 +1,9 @@
+function openDonationPopup() {
+    document.getElementById("donationPopup").style.display = "block"; //initiates popup item
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeDonationPopup() {
+    document.getElementById("donationPopup").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
